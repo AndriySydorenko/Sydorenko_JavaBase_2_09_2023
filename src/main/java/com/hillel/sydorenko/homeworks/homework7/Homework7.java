6cc0efa503d6fbf9c918bd217195aca6626769ca
Andrii Sydorenko <a.sydorenko9595@gmail.com>
2023-02-23 17:37:25 +0200

    some changes

--- src/main/java/com/hillel/sydorenko/homeworks/homework7/Homework7.java
+++ src/main/java/com/hillel/sydorenko/homeworks/homework7/Homework7.java
@@ -7,19 +7,20 @@ public class Homework7 {
         int secretNumber;
         int userNumber;
         int numberGuesses = 0;
+        int maxGuesses = 4;
 
         secretNumber = (int) (Math.random() * 11);
 
         Scanner scanner = new Scanner(System.in);
 
         System.out.println("AI selected a number between 0 and 10, attempt to guess this number!");
-        System.out.println("You have only 3 attempts!");
+        System.out.println("You have only" + " " + maxGuesses + " " + "attempts!");
 
-        while (numberGuesses < 3) {
+        while (numberGuesses < maxGuesses) {
             numberGuesses++;
             System.out.println("Attempts number " + numberGuesses + ":");
             userNumber = scanner.nextInt();
-//            scanner.nextLine(); // тут можемо додати очистку буфера якщо в подальшому ще будемо використовувати сканер
+//          scanner.nextLine(); // тут можемо додати очистку буфера якщо в подальшому ще будемо використовувати сканер
 
             if (userNumber == secretNumber) {
                 System.out.println("Congratulation! You Win!");
@@ -31,12 +32,50 @@ public class Homework7 {
             } else {
                 System.out.println("AI chose less number.");
             }
-            if (numberGuesses == 3) {
+            if (numberGuesses == maxGuesses) {
                 System.out.println("Sorry, you a not guess.");
                 System.out.println("AI guess number: " + secretNumber);
             }
         }
         scanner.close();
+
+//
+//        int secretNumber;
+//        int userNumber;
+//        int numberGuesses = 0;
+//        int maxGuesses = 3;
+//        boolean correct = false;
+//
+//        secretNumber = (int) (Math.random() * 11);
+//
+//        Scanner scanner = new Scanner(System.in);
+//
+//        System.out.println("AI selected a number between 0 and 10, attempt to guess this number!");
+//        System.out.println("You have only" + " " + maxGuesses + " " + "attempts!");
+//
+//        while (numberGuesses < maxGuesses) {
+//            numberGuesses++;
+//            System.out.println("Attempts number " + numberGuesses + ":");
+//            userNumber = scanner.nextInt();
+//
+//            if (userNumber == secretNumber) {
+//                System.out.println("Congratulation! You Win!");
+//                System.out.println("Your number: " + userNumber);
+//                System.out.println("AI number: " + secretNumber);
+//                correct = true;
+//                break;
+//            } else if (userNumber < secretNumber) {
+//                System.out.println("AI chose bigger number.");
+//            } else {
+//                System.out.println("AI chose less number.");
+//            }
+//        }
+//
+//            if (!correct) {
+//                System.out.println("Sorry, you a not guess.");
+//                System.out.println("AI guess number: " + secretNumber);
+//            }
+//        scanner.close();
 
 
     }
